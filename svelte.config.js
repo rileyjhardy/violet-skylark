cbde1159287d9f7142690cb8d2c98a02810b24fe
Riley Hardy <54298858+rileyjhardy@users.noreply.github.com>
2024-12-07 18:02:53 -0600

    Address paths issue

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/violet-skylark' : ''
+    paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
